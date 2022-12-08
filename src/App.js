ae174f38393aa396a6a6421f68a735f409c41323
Nasikh CL <nasikcl@gmail.com>
2022-12-08 16:41:52 +0530

    changed to rapid api for news

--- src/App.js
+++ src/App.js
@@ -5,28 +5,49 @@ import Card from './components/Card';
 import Navbar from './components/Navbar';
 // 07d70fd9fb4d4e5192e1aa45e5a7b17a
 function App() {
-const [data, setData] = useState([])
-  useEffect(()=>{
-    fetch('https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=07d70fd9fb4d4e5192e1aa45e5a7b17a')
-    .then(res => res.json())
-    .then(data=> setData(data.articles) )
 
+  const NewsAPI = require('newsapi');
+  const newsapi = new NewsAPI('07d70fd9fb4d4e5192e1aa45e5a7b17a');
+
+  const [data, setData] = useState([])
+  useEffect(()=>{
+
+    const options = {
+      method: 'GET',
+      headers: {
+        'X-RapidAPI-Key': '43f1bd7fb7mshf340ae335901eeap185a3cjsn6edb7ac3a71b',
+        'X-RapidAPI-Host': 'contextualwebsearch-websearch-v1.p.rapidapi.com'
+      }
+    };
+    
+    // fetch('https://contextualwebsearch-websearch-v1.p.rapidapi.com/api/Search/WebSearchAPI?q=taylor%20swift&pageNumber=1&pageSize=10&autoCorrect=true', options)
+    //   .then(response => response.json())
+    //   .then(response =>setData(response.value))
+    //   .catch(err => console.error(err));
+  
+    
+    fetch('https://contextualwebsearch-websearch-v1.p.rapidapi.com/api/search/NewsSearchAPI?q=taylor%20swift&pageNumber=1&pageSize=10&autoCorrect=true&fromPublishedDate=null&toPublishedDate=null', options)
+      .then(response => response.json())
+      .then(response =>setData(response.value))
+      .catch(err => console.error(err));
+  
   },[])
 
+// const [dd, setdd] = useState([])
 
-  console.log(data)
+  // console.log(data)
   return (
     <div className="App">
       <Navbar />
       <div className='blogs'>
-      { data && data.length > 0 ? data.map((item, index) =>{
+      { (data && data.length > 0)? data.map((item, index) =>{
           return(
             <Card key={index}  item={item} /> 
           )
         })  : <h2>Loading...</h2>}
       </div>
     </div>
-  );
+  ); 
 }
 
 export default App;
--- src/components/Card.js
+++ src/components/Card.js
@@ -6,7 +6,7 @@ const Card = ({item}) => {
     <a href={item.url}>
       <div className='blog-card'>
         <div className='hover-text'><h2>Read Blog</h2></div>
-        <img src={item.urlToImage} />
+        <img src={item.image.url} alt="article_image" />
         <div className='blog-details'>
           <h5>{item.title}</h5>
           <p>{item.description}</p>
