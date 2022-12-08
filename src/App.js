adf23e5a83b80067626a58ccc29964033352246e
Nasikh CL <nasikcl@gmail.com>
2022-12-08 11:34:17 +0530

    blog-page initial setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+
+import { useEffect, useState } from 'react';
 import './App.css';
+import Card from './components/Card';
+import Navbar from './components/Navbar';
 
 function App() {
+const [data, setData] = useState([])
+  useEffect(()=>{
+    fetch('https://newsapi.org/v2/top-headlines?country=us&apiKey=300be0372ce64ead840346911752051c')
+    .the n(res => res.json())
+    .then(data=> setData(data) )
+
+  },[])
+
+  console.log(data)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <Card />
     </div>
   );
 }
