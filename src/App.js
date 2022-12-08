4555567cc12d3f21081924d2adb6be21b820ab57
Nasikh CL <nasikcl@gmail.com>
2022-12-08 13:12:00 +0530

    blog api changed

--- src/App.js
+++ src/App.js
@@ -7,24 +7,23 @@ import Navbar from './components/Navbar';
 function App() {
 const [data, setData] = useState([])
   useEffect(()=>{
-    fetch('https://newsapi.org/v2/everything?q=tesla&from=2022-11-08&sortBy=publishedAt&apiKey=07d70fd9fb4d4e5192e1aa45e5a7b17a')
+    fetch('https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=07d70fd9fb4d4e5192e1aa45e5a7b17a')
     .then(res => res.json())
     .then(data=> setData(data.articles) )
 
   },[])
 
-  const items = data.map((item, index) =>{
-    return(
-      <Card key={index}  item={item} /> 
-    )
-  }) 
 
   console.log(data)
   return (
     <div className="App">
       <Navbar />
       <div className='blogs'>
-      {data.length > 0 ? items : <h2>Loading...</h2>}
+      {data.length > 0 ? data.map((item, index) =>{
+          return(
+            <Card key={index}  item={item} /> 
+          )
+        })  : <h2>Loading...</h2>}
       </div>
     </div>
   );
